90f744b370c5a3614606f7d0e1824e239aadb3e1
GALLOIS, Guillaume <guillaume.gallois@gmail.com>
2023-11-02 16:13:14 +0100

    add test with JsonRootName and Swagger still working

--- src/main/java/com/example/swagger/test/swaggertest/Greeting.java
+++ src/main/java/com/example/swagger/test/swaggertest/Greeting.java
@@ -1,11 +1,13 @@
 package com.example.swagger.test.swaggertest;
 
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonRootName(value = "hello")
 public class Greeting {
 
     private long id;
--- src/main/java/com/example/swagger/test/swaggertest/HelloController.java
+++ src/main/java/com/example/swagger/test/swaggertest/HelloController.java
@@ -1,5 +1,10 @@
 package com.example.swagger.test.swaggertest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +20,11 @@ public class HelloController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private final ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE);
+
+    @Operation(description = "Hello World operation")
     @GetMapping
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public String greeting(@Parameter(description = "parameter value", example = "World") @RequestParam(value = "name", defaultValue = "World") String name) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(new Greeting(counter.incrementAndGet(), String.format(template, name)));
     }
 }
